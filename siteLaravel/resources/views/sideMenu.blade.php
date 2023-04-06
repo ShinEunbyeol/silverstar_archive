e12ad90b0a144dc8540d71d02726033c25ce2bb0
ShinEunbyeol <silverbyeol97@gmail.com>
2023-04-06 23:20:00 +0900

    사이드메뉴 클래스 추가 및 보정

--- siteLaravel/resources/views/sideMenu.blade.php
+++ siteLaravel/resources/views/sideMenu.blade.php
@@ -1,20 +1,20 @@
 <link rel="stylesheet" href="{{ url('css/sideMenu.css') }}" />
 
-<div id="intro-side">
+<div id="intro-side" class="activeMenu eachMenu">
     <div id="eunbyeolShin" class="eachSideMenu">Eunbyeol Shin</div>
     <div id="skill" class="eachSideMenu">Skill</div>
     <div id="career" class="eachSideMenu">Career</div>
     <div id="contact" class="eachSideMenu">Contact</div>
 </div>
 
-<div id="api-side">
-
+<div id="api-side" class="eachMenu">
+    openAPI 사이드
 </div>
 
-<div id="html_css-side">
-
+<div id="html_css-side" class="eachMenu">
+    HTML/CSS 사이드
 </div>
 
-<div id="toy_project-side">
-
+<div id="toy_project-side" class="eachMenu">
+    Toy Project
 </div>
