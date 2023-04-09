bc4cd354c7ae21199d6e0b9d2af22dbc0a2dbce2
ShinEunbyeol <silverbyeol97@gmail.com>
2023-04-09 21:49:33 +0900

    푸터 height 사이즈 변경

--- siteLaravel/resources/views/footer.blade.php
+++ siteLaravel/resources/views/footer.blade.php
@@ -1,11 +1,13 @@
 <link rel="stylesheet" href="{{ url('css/footer.css') }}" />
 
-<div class="nameInfo">
-    신은별
-    <div class="nicknameInfo sideBySide black3 fontSmallBig ml10">
-        @시애닝
+<div class="footerInfo">
+    <div class="nameInfo">
+        신은별
+        <div class="nicknameInfo sideBySide black3 fontSmallBig ml10">
+            @시애닝
+        </div>
     </div>
-</div>
-<div class="emailInfo fontSmallBig">
-    silverbyeol97@gmail.com
+    <div class="emailInfo fontSmallBig">
+        silverbyeol97@gmail.com
+    </div>
 </div>
