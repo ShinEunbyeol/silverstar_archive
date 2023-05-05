5f9a3e275ebf74fd94bbd6655aee9579edb2aa22
ShinEunbyeol <silverbyeol97@gmail.com>
2023-05-06 08:15:39 +0900

    [CDN 라이브러리 추가] - font awesome 6, Sweet Alert

--- siteLaravel/resources/views/index.blade.php
+++ siteLaravel/resources/views/index.blade.php
@@ -14,6 +14,13 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+
+    {{-- font awesome 6 --}}
+    <script src="https://kit.fontawesome.com/9af4859f81.js" crossorigin="anonymous"></script>
+
+    {{-- Sweet Alert --}}
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
     <link rel="stylesheet" href="{{ url('css/main.css') }}" />
     @yield('css')
 
