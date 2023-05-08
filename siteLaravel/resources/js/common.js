d056c6e67b5e40a5c2ea2443aa2ff39691ff6c67
ShinEunbyeol <silverbyeol97@gmail.com>
2023-05-08 18:18:55 +0900

    Date 형식 변경 함수 추가

--- siteLaravel/resources/js/common.js
+++ siteLaravel/resources/js/common.js
@@ -15,3 +15,53 @@ export function get_firstChild(element) {
 
     return firstChild;
 }
+
+/**
+ * TZDate를 문자열(배열) 형식으로 변경
+ * @param {TZDate} date TZDate 형식의 날짜 값
+ * @param {String} type 리턴할 날짜의 형식
+ *                      date: Y-m-d
+ *                      datetime: Y-m-d H:i:s
+ *                      ym: y-m
+ *                      md: m-d
+ *                      element: 각 연,월,일,시,분,초의 문자열 배열
+ * @returns 문자열 또는 문자열 배열의 날짜 리턴
+ */
+export function transition_tzdateToYmd(date, type) {
+    type = typeof type !== 'undefined' ? type : 'datetime';
+
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const second = date.getSeconds();
+
+    let result;
+    switch(type) {
+        case 'date':
+            result = year + '-' + month + '-' + day;
+            break;
+        case 'datetime':
+            result = year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
+            break;
+        case 'ym':
+            result = year + '-' + month;
+            break;
+        case 'md':
+            result = month + '-' + day;
+            break;
+        case 'element':
+            result = {
+                'year': year,
+                'month': month,
+                'day': day,
+                'hour': hour,
+                'minute': minute,
+                'second': second
+            }
+            break;
+    }
+
+    return result;
+}
